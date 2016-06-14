ce5560e8e17bd2714dd5c9b2854a3540b3f2ce27
Dan <dancannova@gmail.com>
2016-06-14 16:16:15 -0700

    Using jQuerry to greet

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var g = G$('John', 'Doe');
 
-g.greet().setLanguage('es').greet(true).log();
+//g.greet().setLanguage('es').greet(true).log();
+
+$('#login').click(function(){
+   var loginGrts = G$('John', 'Doe');
+
+    $('#logindiv').hide();
+
+    loginGrts.setLanguage($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
